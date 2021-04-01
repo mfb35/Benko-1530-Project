5a9be6e0225aabf3455bb7dba8e5d4112bf40510
Maxwell Benko <47008373+mfb35@users.noreply.github.com>
2021-04-01 17:14:44 -0400

    Almost done with posts page

--- campaign-posts/src/App.js
+++ campaign-posts/src/App.js
@@ -11,9 +11,10 @@ function App() {
     return (
         <Router>
             <div className="App">
-                <Route path='/' exact render={(props) => (
+                <Route path='/' exact render={(props) => (//this is the route for the campaigns page
                     <>
-                        <a href='/home'>home</a>
+                        <br /> <a href='/Home'>Home</a><br />
+                        <a href='/Create'>Create a Campaign</a>
                         <h1 style={{ color: 'red' }}>CharityDrop Campaigns</h1>
                         <input
                             type="text"
@@ -35,7 +36,7 @@ function App() {
                             return (
                                 <div className="user" key={key}>
                                     <Image img={val.PFP} />
-                                    <a href={'/Campaign/'+val.campaign}>{val.campaign}</a>
+                                    <a href={'/Campaign/'+val.campaign}>{val.campaign} </a>
                                     <p><h5> Description: </h5>{val.Description}</p>
                                     <ColoredLine color="black" />
                                 </div>
@@ -43,10 +44,31 @@ function App() {
                         })}
                     </>
                 )} />
-                <Route path='/' />           
+                <Route path='/' />
+
+                <Route exact render={(props) => (//this is the route for all of the links shown on the campaigns page
+                    < h1 style={{ color: 'red' }}> {getURL()} </h1>
+                )} />
+                <Route path='/home' />
+                
             </div>
         </Router>
     );
 }
 
+function getURL() {
+    let currentURL = window.location.href.substring(22).replace(/%/g, ' ').replace(/20/g, '')//magic number of 22 will have to be changed later
+    if (currentURL.includes("Campaign/")) {
+        currentURL = currentURL.substring(9)
+    }
+    return currentURL
+}
+
+function getCampaignAttributes() {
+    for (var i = 0; i < JSONDATA.map.length; i++) {
+        if (JSONDATA[i].campaign == getURL()) {
+            return JSONDATA[i];
+        }
+    }
+}
 export default App;
